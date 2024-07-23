063db64d004f567eda899dfd3bc550f322bb1a2f
harshal342 <harshalwagh342@gmail.com>
2024-07-23 17:17:57 +0530

    Cakes blogs added

---
+++ src/views/BlogView/BlogView.js
@@ -0,0 +1,28 @@
+import "./BlogView.css"
+import { useParams } from "react-router-dom"
+import blogs from "../../data"
+
+
+function BlogView() {
+    const {id} = useParams()
+
+    const selectedBlog = blogs.find((blogObject)=> blogObject.id === id
+    )
+    console.log(selectedBlog);
+  return (
+        <div className="BlogView-cards">   
+            <h1>{selectedBlog.title}</h1>
+            <p className="BlogView-author-details">
+                <img src={selectedBlog.author.avtar} alt="avtar" height="90px" className="BlogView-avtar"/>
+                {selectedBlog.author.name} | {selectedBlog.date}
+            </p>
+            <img src={selectedBlog.imgURL} className="BlogView-img"/> <br/> <br/>
+            {selectedBlog.content}
+            <p className="price">{selectedBlog.price}</p>
+            
+        </div>
+
+    )
+}
+
+export default BlogView
